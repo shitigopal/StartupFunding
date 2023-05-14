c8045d141d6762458434d2116b638f986989c899
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-14 16:07:15 +0530

    ..

--- frontend/src/components/main/StartupDetails.js
+++ frontend/src/components/main/StartupDetails.js
@@ -1,30 +1,276 @@
-import React from 'react'
+import { Rating } from "@mui/material";
+import { Formik } from "formik";
+import { MDBInput } from "mdb-react-ui-kit";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import app_config from "../../config";
+import subscriptionData from "../../subscriptionDetails";
 
 const StartupDetails = () => {
+  const { id } = useParams();
+  const [rating, setRating] = useState(4);
+  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem('startup')));
+  const [currentInvestor, setCurrentInvestor] = useState(JSON.parse(sessionStorage.getItem('investor')));
+  const [feedbackList, setFeedbackList] = useState([]);
+  const navigate = useNavigate();
+  const [planDetails, setPlanDetails] = useState(null);
 
-    
+  // console.log(id);
 
-  return (
-    <div>
-     <div className="card" style={{ width: "18rem" }}>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p className="card-text">
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </p>
-    <a href="#" className="card-link">
-      Card link
-    </a>
-    <a href="#" className="card-link">
-      Another link
-    </a>
-  </div>
-</div>
+  const [startupData, setStartupData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const url = app_config.apiurl;
 
-    </div>
-  )
-}
+  const fetchStartupById = async () => {
+    setLoading(true);
+    const res = await fetch("http://localhost:5000/startup/getbyid/" + id);
+    const data = await res.json();
+    console.log(data);
+    fetchStartupFeedbacks(data.result._id);
+    setStartupData(data.result);
+    setLoading(false);
+  };
 
-export default StartupDetails
+  useEffect(() => {
+    fetchStartupById();
+    if(currentInvestor){
+      getSubscriptionData(currentInvestor._id);
+    }else{
+      getSubscriptionData(currentUser._id);
+
+    }
+  }, []);
+
+  const feedbackSubmit = async (formdata, { setSubmitting }) => {
+    let loggedinUser = currentUser !== null ? currentUser:currentInvestor;
+    if (!loggedinUser) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Login needed!',
+      })
+      console.log('startup login needed');
+      return;
+    }
+    formdata.user = loggedinUser._id;
+    formdata.rating = rating;
+    formdata.created_at = new Date();
+    setSubmitting(true);
+    const res = await fetch(url+`/feedback/add`, {
+      method: "POST",
+      body: JSON.stringify(formdata),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log(res.status)
+    setSubmitting(false);
+    fetchStartupFeedbacks(startupData._id);
+  }
+
+  const fetchStartupFeedbacks = async (id) => {
+    const res = await fetch(`http://localhost:5000/feedback/getbystartup/` + id);
+    const data = (await res.json()).result;
+    console.log(data);
+    setFeedbackList(data);
+  }
+
+  const displayFeedbacks = () => {
+    const reversedList = feedbackList.map((_, index) => feedbackList[feedbackList.length - 1 - index]);
+
+    return reversedList.map((feedback) => (
+      <div className="border rounded card m-1 ps-5">
+
+        <h5 className="pt-2">{feedback.user.name}</h5>
+        <p className="m-0 " style={{ fontSize: 10 }}>{new Date(feedback.created_at).toLocaleDateString()}</p>
+
+        <Rating
+          value={feedback.rating}
+          readOnly
+        />
+        <p>{feedback.content}</p>
+      </div>
+    ))
+  }
+
+  const openChat = () => {
+    if(currentInvestor){
+        checkVisiblity('chat', '/investor/chat/' + startupData._id)
+    }
+    else if(currentUser){
+        checkVisiblity('chat', '/startup/chat/' + startupData._id)   
+    }
+  }
+
+  const getSubscriptionData = async (id) => {
+    const res = await fetch(url + '/Subscription/getbyuser/' + id);
+    if (res.status === 201) {
+      const data = await res.json();
+      console.log(data.result);
+      setPlanDetails(data.result);
+    }
+  }
+
+  const checkVisiblity = (feature, path) => {
+    if(!planDetails){
+      Swal.fire({title : 'You need to subscribe!!'})
+      return
+    }
+    if (subscriptionData[planDetails.data.plan.name].includes(feature)) { navigate(path) }
+    else Swal.fire({ title: 'Please upgrade your plan!!' })
+  }
+
+  const displayDetails = () => {
+    if (!loading && startupData) {
+      return <div
+        className="col card mx-auto m-3 p-2"
+        style={{ width: "80%", height: "fit-content" }}
+      >
+        {/* startup  */}
+        <div className="row justify-content-center">
+          <img
+            className="rounded-circle shadow-1-strong mb-4"
+            src={url+'/'+startupData.startupimage} 
+            alt="avatar"
+            style={{ width: 150, color: "" }}
+          />
+        </div>
+        <div className="text-center row">
+          <h5>{startupData.name}</h5>
+          <div className="d-flex flex-row justify-content-center">
+            <p className="me-4">
+              <i class="fas fa-mail-bulk fa-lg  "></i> &nbsp; {startupData.email}
+            </p>
+
+            <p>
+              <i class="fab fa-linkedin fa-lg  "></i> &nbsp; {startupData.name}
+            </p>
+          </div>
+
+          <p>
+            <i class="fas fa-quote-left    "></i>&nbsp; {startupData.brief}
+          </p>
+        </div>
+
+        <div className="row d-flex flex-row justify-content-center mt-5">
+          {/* founder  */}
+          <div className="col-lg-6  d-flex flex-row justify-content-center">
+            <div className="me-5">
+              <img
+                className="mx-auto"
+                src={url+'/'+startupData.owneravatar} 
+                alt=""
+                style={{ width: "100px", borderRadius: "10px" }}
+              />
+            </div>
+            <div>
+              <h5>{startupData.ownername}</h5>
+              <p>
+                Founder and CEO of <strong>{startupData.name}</strong>
+              </p>
+              <p className="me-4">
+                {" "}
+                <i class="fas fa-mail-bulk fa-lg  "></i> &nbsp;
+                ollaheden@gmail.com
+              </p>
+            </div>
+          </div>
+          {/* product  */}
+          <div
+            className="col-lg-6 d-flex flex-row justify-content-center "
+            style={{ maxWidth: "400px" }}
+          >
+            <div className="me-5 ">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQbuMqNab_lb7R8vMfZ9r2lA0UVHL7lFH7nA&usqp=CAU"
+                alt=""
+                style={{ width: "100px", borderRadius: "10px" }}
+              />
+            </div>
+
+            <div>
+              <h5>{startupData.productname}</h5>
+              <p>
+              
+              {startupData.productdescription}
+                
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <center>
+          <button className="btn m-5" style={{ backgroundColor: "#9c3353", color: "white", width: "fit-content" }} onClick={openChat}>Start Chatting</button>
+        </center>
+        <hr />
+        <div style={{ backgroundColor: "#f0efef" }}>
+          <h2 className="text-center pt-5">Write your reviews</h2>
+
+          <div className="mt-2 p-5">
+
+            <Rating
+              name="Feedback"
+              value={rating}
+              onChange={(event, newValue) => {
+                setRating(newValue);
+              }}
+            />
+            <Formik
+              initialValues={{
+                content: "",
+                startup: startupData._id
+              }}
+              onSubmit={feedbackSubmit}>
+
+              {({ values, handleSubmit, handleChange, isSubmitting, }) => (
+
+                <form onSubmit={handleSubmit}  >
+                  {/* 2 column grid layout with text inputs for the first and last names */}
+                  <div className="row ">
+                    <div className="col-md-10">
+
+
+                      <MDBInput
+                        label="Write your feedback"
+                        type="text"
+                        value={values.content}
+                        onChange={handleChange}
+                        name="content"
+                      />
+
+                    </div>
+
+                    <div className="col-md-2">
+                      <button className="btn " style={{ backgroundColor: "#9c3353", color: "white" }}>Submit</button>
+                    </div>
+
+                  </div>
+
+
+
+
+
+                </form>
+              )}
+            </Formik>
+
+          </div>
+        </div>
+        {displayFeedbacks()}
+
+      </div>
+
+
+
+    }
+    else {
+      return <div className='text-center'><img src="https://cdn.dribbble.com/users/3533804/screenshots/6666006/all-anim-gif.gif" style={{ width: "500px" }} alt="" />
+        <h1 style={{ color: "#518f3d" }}>Loading...</h1></div>
+    }
+  };
+
+  return displayDetails()
+};
+
+export default StartupDetails;
--- frontend/src/components/main/StartupListing.js
+++ frontend/src/components/main/StartupListing.js
@@ -1,274 +1,126 @@
-import React, { useState } from 'react'
-import app_config from '../../config';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import app_config from "../../config";
 
 const StartupListing = () => {
-
-  const {apiurl} = app_config;
   const [startupList, setStartupList] = useState([]);
+  
+  const [currentUser, setCurrentUser] = useState(JSON.parse(sessionStorage.getItem(null)));
+  const [searchKeyword, setSearchKeyword] = useState("");
 
-  const getStartupsData = async () => {
-    const res = fetch(apiurl+'/')
+
+  const filters = [
+    {
+      name : 'Sector',
+      option: [
+        'technology',
+        'entertainment',
+        'Art And Photography',
+        'Technology',
+        'others',
+      ]
+    }
+  ]
+
+  const search =  async (field) => {
+    const res = await fetch(url + "/startup/getall");
+    const data = await res.json();
+    console.log(data);
+    setStartupList(data.result.filter((user) => (user.role === "startup" 
+    && user[field] === searchKeyword)));
   }
 
+
+  const checkSubscription = async () => {
+    const res = await fetch(url+`/Subscription/getbyuser/`);   
+      if(currentUser!==null){
+          if(currentUser.data.name === "Basic")
+          {
+            const data = await res.json();
+          }
+      }
+  }
+  
+ 
+  const url = app_config.apiurl;
+  const placeholder = 'https://via.placeholder.com/640x400.png/a59090/000000?Text=640x400';
+
+  const fetchData = async () => {
+    const res = await fetch(url + "/startup/getall");
+    const data = await res.json();
+    console.log(data);
+    setStartupList(data.result.filter((user) => user.role === "startup"));
+    // setStartupList(data.result);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const displayList = () => {
+    
+    return startupList.map((startup) => (
+      
+      <div className="card mb-3 mx-auto" style={{width:"80%"}}>
+        <div className="row gx-5 justify-content-center">
+          <div className="col-md-4">
+            <img
+              className="img-fluid p-2"
+              src={startup.startupimage ? (url + "/" + startup.startupimage) : placeholder}
+              alt=""
+              style={{maxHeight:"200px",width:"200px"}}
+            />
+          </div>
+
+          <div className="col-md-6">
+            <div className="card-body">
+              <h4>
+                <strong> {startup.name} </strong>
+              </h4>
+              <p className="text-muted">{startup.email}</p>
+              <Link
+                type="button"
+                className="btn"
+                to={"/main/startupdetails/" + startup._id}
+                style={{ backgroundColor: "#9c3353", color: "white" }}
+              >
+                Read more
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    ));
+  };
+
   return (
-    <section style={{ backgroundColor: "#eee" }}>
-  <div className="container py-5">
-    <div className="row justify-content-center mb-3">
-      <div className="col-md-12 col-xl-10">
-        <div className="card shadow-0 border rounded-3">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
-                <div className="bg-image hover-zoom ripple rounded ripple-surface">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/img%20(4).webp"
-                    className="w-100"
-                  />
-                  <a href="#!">
-                    <div className="hover-overlay">
-                      <div
-                        className="mask"
-                        style={{ backgroundColor: "rgba(253, 253, 253, 0.15)" }}
-                      />
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-6 col-xl-6">
-                <h5>Quant trident shirts</h5>
-                <div className="d-flex flex-row">
-                  <div className="text-danger mb-1 me-2">
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                  </div>
-                  <span>310</span>
-                </div>
-                <div className="mt-1 mb-0 text-muted small">
-                  <span>100% cotton</span>
-                  <span className="text-primary"> • </span>
-                  <span>Light weight</span>
-                  <span className="text-primary"> • </span>
-                  <span>
-                    Best finish
-                    <br />
-                  </span>
-                </div>
-                <div className="mb-2 text-muted small">
-                  <span>Unique design</span>
-                  <span className="text-primary"> • </span>
-                  <span>For men</span>
-                  <span className="text-primary"> • </span>
-                  <span>
-                    Casual
-                    <br />
-                  </span>
-                </div>
-                <p className="text-truncate mb-4 mb-md-0">
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomised words which don't look
-                  even slightly believable.
-                </p>
-              </div>
-              <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
-                <div className="d-flex flex-row align-items-center mb-1">
-                  <h4 className="mb-1 me-1">$13.99</h4>
-                  <span className="text-danger">
-                    <s>$20.99</s>
-                  </span>
-                </div>
-                <h6 className="text-success">Free shipping</h6>
-                <div className="d-flex flex-column mt-4">
-                  <button className="btn btn-primary btn-sm" type="button">
-                    Details
-                  </button>
-                  <button
-                    className="btn btn-outline-primary btn-sm mt-2"
-                    type="button"
-                  >
-                    Add to wishlist
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="row justify-content-center mb-3">
-      <div className="col-md-12 col-xl-10">
-        <div className="card shadow-0 border rounded-3">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
-                <div className="bg-image hover-zoom ripple rounded ripple-surface">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(4).webp"
-                    className="w-100"
-                  />
-                  <a href="#!">
-                    <div className="hover-overlay">
-                      <div
-                        className="mask"
-                        style={{ backgroundColor: "rgba(253, 253, 253, 0.15)" }}
-                      />
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-6 col-xl-6">
-                <h5>Quant olap shirts</h5>
-                <div className="d-flex flex-row">
-                  <div className="text-danger mb-1 me-2">
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                  </div>
-                  <span>289</span>
-                </div>
-                <div className="mt-1 mb-0 text-muted small">
-                  <span>100% cotton</span>
-                  <span className="text-primary"> • </span>
-                  <span>Light weight</span>
-                  <span className="text-primary"> • </span>
-                  <span>
-                    Best finish
-                    <br />
-                  </span>
-                </div>
-                <div className="mb-2 text-muted small">
-                  <span>Unique design</span>
-                  <span className="text-primary"> • </span>
-                  <span>For men</span>
-                  <span className="text-primary"> • </span>
-                  <span>
-                    Casual
-                    <br />
-                  </span>
-                </div>
-                <p className="text-truncate mb-4 mb-md-0">
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomised words which don't look
-                  even slightly believable.
-                </p>
-              </div>
-              <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
-                <div className="d-flex flex-row align-items-center mb-1">
-                  <h4 className="mb-1 me-1">$14.99</h4>
-                  <span className="text-danger">
-                    <s>$21.99</s>
-                  </span>
-                </div>
-                <h6 className="text-success">Free shipping</h6>
-                <div className="d-flex flex-column mt-4">
-                  <button className="btn btn-primary btn-sm" type="button">
-                    Details
-                  </button>
-                  <button
-                    className="btn btn-outline-primary btn-sm mt-2"
-                    type="button"
-                  >
-                    Add to wishlist
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="row justify-content-center">
-      <div className="col-md-12 col-xl-10">
-        <div className="card shadow-0 border rounded-3">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
-                <div className="bg-image hover-zoom ripple rounded ripple-surface">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/new/img(5).webp"
-                    className="w-100"
-                  />
-                  <a href="#!">
-                    <div className="hover-overlay">
-                      <div
-                        className="mask"
-                        style={{ backgroundColor: "rgba(253, 253, 253, 0.15)" }}
-                      />
-                    </div>
-                  </a>
-                </div>
-              </div>
-              <div className="col-md-6 col-lg-6 col-xl-6">
-                <h5>Quant ruybi shirts</h5>
-                <div className="d-flex flex-row">
-                  <div className="text-danger mb-1 me-2">
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                    <i className="fa fa-star" />
-                  </div>
-                  <span>145</span>
-                </div>
-                <div className="mt-1 mb-0 text-muted small">
-                  <span>100% cotton</span>
-                  <span className="text-primary"> • </span>
-                  <span>Light weight</span>
-                  <span className="text-primary"> • </span>
-                  <span>
-                    Best finish
-                    <br />
-                  </span>
-                </div>
-                <div className="mb-2 text-muted small">
-                  <span>Unique design</span>
-                  <span className="text-primary"> • </span>
-                  <span>For women</span>
-                  <span className="text-primary"> • </span>
-                  <span>
-                    Casual
-                    <br />
-                  </span>
-                </div>
-                <p className="text-truncate mb-4 mb-md-0">
-                  There are many variations of passages of Lorem Ipsum
-                  available, but the majority have suffered alteration in some
-                  form, by injected humour, or randomised words which don't look
-                  even slightly believable.
-                </p>
-              </div>
-              <div className="col-md-6 col-lg-3 col-xl-3 border-sm-start-none border-start">
-                <div className="d-flex flex-row align-items-center mb-1">
-                  <h4 className="mb-1 me-1">$17.99</h4>
-                  <span className="text-danger">
-                    <s>$25.99</s>
-                  </span>
-                </div>
-                <h6 className="text-success">Free shipping</h6>
-                <div className="d-flex flex-column mt-4">
-                  <button className="btn btn-primary btn-sm" type="button">
-                    Details
-                  </button>
-                  <button
-                    className="btn btn-outline-primary btn-sm mt-2"
-                    type="button"
-                  >
-                    Add to wishlist
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+    <div>
 
-  )
-}
+      <div style={{ backgroundColor: "#9c3353", padding: '50px 0',height:"40vh"}}>
+        <div className="row mt-4 ">
 
-export default StartupListing
+          <div className="col-md-8 mx-auto mt-1">
+            <h1 className="text-white text-center">Startup List</h1>
+            <div className="input-group my-3 px-3 pt-4">
+              <input className="form-control  p-3" value={searchKeyword} onChange={e => setSearchKeyword(e.target.value)} />
+              <button className="btn btn-primary input-group-append" onClick={e => search('name')}>Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <Link className="btn mt-5 mb-3" 
+      to="/startup/dashboard"
+      style={{background:"#9c3353",color:"white",width:"fit-content",height:"fit-content",marginLeft:"20%"}}
+      >Back</Link>
+
+      <div className="row justify-content-center">
+      
+        <div className="col-9">{displayList()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default StartupListing;
