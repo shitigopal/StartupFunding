6fcdbff5682599ce431e8b899023a3cc6d0081a8
shitigopal <97109154+shitigopal@users.noreply.github.com>
2023-05-14 14:57:49 +0530

    ...

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -78,19 +78,19 @@ const Home = () => {
             <div className="homepage">
                     <h2> why it is so great?</h2>
                    
-  //   // return (
-  //     <div>
-  //       {/* Hello world */}
-  //       <div className="awesome" style={{border: '1px solid red'}}>
-  //         <label htmlFor="name">Enter your name: </label>
-  //         <input type="text" id="name" />
-  //       </div>
-  //       <p>Enter your HTML here</p>
-  //     </div>
-  //   );
-  {/* // } */}
-
-                  </div>
+      <div>
+        {/* Hello world */}
+        <div className="awesome" style={{border: '1px solid red'}}>
+          <label htmlFor="name">Enter your name: </label>
+          <input type="text" id="name" />
+        </div>
+        <div className="awesome" style={{border: '1px solid red'}}>
+          <label htmlFor="name">Enter your HTML: </label>
+          <input type="text" id="name" />
+        </div>
+        
+      </div>
+          </div>
           </section>
           
           {/**/}
